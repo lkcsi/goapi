611abac844d7c7a84922746d111115bbeb46b1f0
roland <lukacsiroland01@gmail.com>
2024-01-05 20:16:54 +0100

    Add checkout and delete endpoints

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"log"
+	"strconv"
 
 	"net/http"
 
@@ -10,17 +11,17 @@ import (
 )
 
 type book struct {
-	Id       string `json:"id"`
+	Id       int    `json:"id"`
 	Title    string `json:"title"`
 	Author   string `json:"author"`
-	Quantity string `json:"quantity"`
+	Quantity int    `json:"quantity"`
 }
 
 var books = []book{
-	{Id: "1", Title: "Title_1", Author: "Author_1", Quantity: "Quantity_1"},
-	{Id: "2", Title: "Title_2", Author: "Author_2", Quantity: "Quantity_2"},
-	{Id: "3", Title: "Title_3", Author: "Author_3", Quantity: "Quantity_3"},
-	{Id: "4", Title: "Title_4", Author: "Author_4", Quantity: "Quantity_4"},
+	{Id: 1, Title: "Title_1", Author: "Author_1", Quantity: 1},
+	{Id: 2, Title: "Title_2", Author: "Author_2", Quantity: 3},
+	{Id: 3, Title: "Title_3", Author: "Author_3", Quantity: 0},
+	{Id: 4, Title: "Title_4", Author: "Author_4", Quantity: 4},
 }
 
 func getBooks(context *gin.Context) {
@@ -30,22 +31,70 @@ func getBooks(context *gin.Context) {
 func createBook(context *gin.Context) {
 	var newBook book
 	if err := context.BindJSON(&newBook); err != nil {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 	books = append(books, newBook)
 	context.IndentedJSON(http.StatusCreated, newBook)
 }
 
+func deleteById(context *gin.Context) {
+	str := context.Param("id")
+	id, err := strconv.Atoi(str)
+	if err != nil {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	index, err := findBookIndex(id)
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
+	books = append(books[:index], books[index+1:]...)
+	context.IndentedJSON(http.StatusNoContent, nil)
+}
+func checkoutBook(context *gin.Context) {
+	str := context.Param("id")
+	id, err := strconv.Atoi(str)
+	if err != nil {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	book, err := findBookById(id)
+	if book.Quantity <= 0 {
+		context.IndentedJSON(http.StatusBadGateway, gin.H{"message": "out of stock"})
+		return
+	}
+
+	book.Quantity -= 1
+	context.IndentedJSON(http.StatusOK, gin.H{"quantity": book.Quantity})
+}
+
 func getBookById(context *gin.Context) {
-	id := context.Param("id")
+	str := context.Param("id")
+	id, err := strconv.Atoi(str)
+	if err != nil {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
 	book, err := findBookById(id)
 	if err != nil {
-		context.IndentedJSON(http.StatusNotFound, err)
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
 	}
 	context.IndentedJSON(http.StatusOK, book)
 }
 
-func findBookById(id string) (*book, error) {
+func findBookIndex(id int) (int, error) {
+	for i, book := range books {
+		if book.Id == id {
+			return i, nil
+		}
+	}
+	return 0, errors.New("book not found")
+}
+
+func findBookById(id int) (*book, error) {
 	for i, book := range books {
 		if book.Id == id {
 			return &books[i], nil
@@ -58,8 +107,12 @@ func main() {
 	log.Println("Application started")
 
 	server := gin.Default()
+
 	server.GET("/books", getBooks)
 	server.GET("/books/:id", getBookById)
+	server.DELETE("/books/:id", deleteById)
 	server.POST("/books", createBook)
+	server.PATCH("/checkout/:id", checkoutBook)
+
 	server.Run("localhost:8080")
 }
